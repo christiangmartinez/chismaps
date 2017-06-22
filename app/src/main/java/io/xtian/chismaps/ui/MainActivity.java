edc7daedccdd703bd80c881d61b75e25101becee
Christian Martinez <cgmartinez11142@gmail.com>
2017-06-22 10:07:39 -0700

    add click functionality to new comment button

--- app/src/main/java/io/xtian/chismaps/ui/MainActivity.java
+++ app/src/main/java/io/xtian/chismaps/ui/MainActivity.java
@@ -1,15 +1,32 @@
 package io.xtian.chismaps.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.xtian.chismaps.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.newComment) ImageView mNewComment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mNewComment.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mNewComment) {
+            Intent intent = new Intent(MainActivity.this, NewCommentActivity.class);
+            startActivity(intent);
+        }
     }
 }
