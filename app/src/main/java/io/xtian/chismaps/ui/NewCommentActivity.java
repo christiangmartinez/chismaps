abf71c2af6a83f8d6329c52c948914d76a332206
Christian Martinez <cgmartinez11142@gmail.com>
2017-06-22 10:54:01 -0700

    add text field and button to new comment activity

--- app/src/main/java/io/xtian/chismaps/ui/NewCommentActivity.java
+++ app/src/main/java/io/xtian/chismaps/ui/NewCommentActivity.java
@@ -1,15 +1,35 @@
 package io.xtian.chismaps.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.xtian.chismaps.R;
 
-public class NewCommentActivity extends AppCompatActivity {
+public class NewCommentActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.newCommentText) EditText mNewCommentText;
+    @Bind(R.id.newCommentButton) Button mNewCommentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_comment);
+        ButterKnife.bind(this);
+        mNewCommentButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mNewCommentButton) {
+            String comment = mNewCommentText.getText().toString();
+            Intent intent = new Intent(NewCommentActivity.this, FeedActivity.class);
+            intent.putExtra("comment", comment);
+            startActivity(intent);
+        }
     }
 }
