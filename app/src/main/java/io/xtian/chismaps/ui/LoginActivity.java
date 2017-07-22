923beea4a7f0472a83050fb96f751379a5f9c2eb
Christian Martinez <cgmartinez11142@gmail.com>
2017-07-22 15:46:41 -0700

    create variables and intents for log in activity

--- app/src/main/java/io/xtian/chismaps/ui/LoginActivity.java
+++ app/src/main/java/io/xtian/chismaps/ui/LoginActivity.java
@@ -1,15 +1,36 @@
 package io.xtian.chismaps.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.xtian.chismaps.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.loginEmail) EditText loginEmail;
+    @Bind(R.id.loginPassword) EditText loginPassword;
+    @Bind(R.id.loginButton) Button loginButton;
+    @Bind(R.id.createNewAccount) TextView createNewAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        createNewAccount.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == createNewAccount) {
+            Intent intent = new Intent (LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+        }
     }
 }
