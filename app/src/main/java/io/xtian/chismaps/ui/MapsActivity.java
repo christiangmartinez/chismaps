741e59cb6fe05cdb3a5ade0f5fe3f498719ac83e
Christian Martinez <cgmartinez11142@gmail.com>
2017-07-22 22:08:46 -0700

    change pin location to portland

--- app/src/main/java/io/xtian/chismaps/ui/MapsActivity.java
+++ app/src/main/java/io/xtian/chismaps/ui/MapsActivity.java
@@ -41,8 +41,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng portland = new LatLng(46, -123);
+        mMap.addMarker(new MarkerOptions().position(portland).title("Marker in Portland"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(portland));
     }
 }
